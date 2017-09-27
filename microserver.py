fcbbfd7c89b4230cfa89d8cdd3f306904419c9cd
ronrest <ronny.coding@gmail.com>
2017-09-27 14:10:47 +1000

    FEAT: Add functions to handle model pages

--- microserver.py
+++ microserver.py
@@ -4,6 +4,7 @@
 """
 from __future__ import print_function, division, unicode_literals
 import os
+import pickle
 
 from flask import Flask
 from flask import render_template, url_for
@@ -11,12 +12,27 @@ app = Flask(__name__)
 
 MODELS_DIR = "../models"
 
+def pickle2obj(file):
+    with open(file, mode="rb") as fileObj:
+        obj = pickle.load(fileObj)
+    return obj
+
+
 def get_best_score(model_name):
     try:
         with open(os.path.join(MODELS_DIR, model_name, "best_score.txt"), mode="r") as f:
             return float(f.read().strip())
     except IOError:
         return(0)
+
+
+def get_evals_dict(model_name):
+    try:
+        evals = pickle2obj(os.path.join(MODELS_DIR, model_name, "evals.pickle"))
+    except:
+        print("WARNING: Could not load {} \n - Returning blank evals dict instead")
+        evals = {"valid_acc": [], "train_acc":[]}
+    return evals
 
 
 @app.route("/")
@@ -28,3 +44,15 @@ def index():
                            model_and_scores=zip(model_names, scores))
 
 
+@app.route('/models/<model_name>')
+def show_user_profile(model_name):
+    score = get_best_score(model_name)
+    evals = get_evals_dict(model_name)
+    return render_template('model.html',
+                           style_path=url_for('static', filename='style.css'),
+                           model_name=model_name,
+                           plot_title="Accuracy over time",
+                           train_data=evals["train_acc"],
+                           valid_data=evals["valid_acc"],
+                           x=list(range(len(evals["valid_acc"]))),
+                           )
